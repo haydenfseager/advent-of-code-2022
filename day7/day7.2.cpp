58dde780433fea9e992d5a6387b2e1b5eb8edd3f
Hayden Seager <118368802+haydenfseager@users.noreply.github.com>
2023-01-19 15:23:20 -0500

    Final Changes

--- day7/day7.2.cpp
+++ day7/day7.2.cpp
@@ -59,7 +59,6 @@ struct Directory {
 
     void printContents(){
         if(folders.size() == 0 && files.size() == 0){
-            cout << "Empty container" << endl;
             return;
         }
         for(int i=0; i<folders.size(); i++){
@@ -87,25 +86,13 @@ int BFS(Directory d){
     return output;
 }
 
-void BFSprint(Directory d){
-    queue<Directory> q;
-    q.push(d);
-    while(!q.empty()){
-        auto curr_dir = q.front();
-        q.pop();
-        curr_dir.printContents();
-        for(int i=0; i<curr_dir.folders.size(); i++){
-            q.push(curr_dir.folders[i]);
-        }
-    }
-}
-
 void runCode(){
+    string line, type;
+    ifstream fin;
     stack<reference_wrapper<Directory>> dir_stack;
     Directory home = Directory("/");
     dir_stack.push(ref(home));
-    string line, type;
-    ifstream fin;
+
     fin.open("day7.txt");
     while(getline(fin, line)){
         stringstream ss(line);
@@ -125,7 +112,6 @@ void runCode(){
                     }
                 }
                 else if(command2 == ".."){
-                    cout << "\tPopping Stack" << endl;
                     dir_stack.pop();
                 }
                 else{
