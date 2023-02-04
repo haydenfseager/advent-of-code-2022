9b8bb25989c7e8fac6bb3266057d7c5868c8ca96
Hayden Seager <118368802+haydenfseager@users.noreply.github.com>
2023-02-04 15:03:31 -0500

    Cleaned up day11

--- day11/day11.2.cpp
+++ day11/day11.2.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int highestVal = 1;
 
 struct Monkey{
-    queue<long long> items;
+    queue<int> items;
     char operation;
     string operationVal;
-    long long numInspected;
+    int numInspected;
     int divisibleBy;
     pair<int, int> nextMonkeyVals;
 
@@ -39,32 +39,11 @@ struct Monkey{
         }
         items.front() %= highestVal;
         if(items.front() % divisibleBy == 0){
-            //cout << "\tThrew to Monkey " << nextMonkeyVals.second << endl;
             return nextMonkeyVals.second;
         }
         else{
-            //cout << "\tThrew to Monkey " << nextMonkeyVals.first << endl;
             return nextMonkeyVals.first;
         }
-    }
-    void printMonkey(){
-        queue<int> temp;
-        cout << "items= ";
-        while(!items.empty()){
-            cout << items.front() << " ";
-            temp.push(items.front());
-            items.pop();
-        }
-        while(!temp.empty()){
-            items.push(temp.front());
-            temp.pop();
-        }
-        cout << endl;
-        cout << "Operation= " << operation << endl;
-        cout << "OperationVal= " << operationVal << endl;
-        cout << "divisibleBy= " << divisibleBy << endl;
-        cout << "nextMonkeyVals= " << nextMonkeyVals.first << " " << nextMonkeyVals.second << endl;
-        cout << endl;
     }
 };
 
@@ -126,28 +105,17 @@ void run(){
         //m->printMonkey(); //Use this for testing
         monkeys.push_back(*m);
     }
-    cout << "highestVal= " << highestVal << endl;
     for(int i=1; i<=10000; i++){
-        //cout << "---------------------------" << endl;
-        //cout << "\tRound " << i << endl;
-        //cout << "---------------------------" << endl;
-        if(i==1 || i==20 || i%1000 == 0){
-            cout << "\ti= " << i << endl;
-        }
         for(int j=0; j<monkeys.size(); j++){
-            //cout << "Monkey " << j << endl;
             while(!monkeys[j].items.empty()){
                 int toMonkey = monkeys[j].nextMonkeyVal();
                 monkeys[toMonkey].items.push(monkeys[j].items.front());
                 monkeys[j].items.pop();
                 monkeys[j].numInspected += 1;
             }
-            if(i==1 || i==20 || i%1000 == 0){
-                cout << j << ": " << monkeys[j].numInspected << endl;
-            }
         }
     }
-    long long first = 0, second = 0;
+    int first = 0, second = 0;
     for(int i=0; i<monkeys.size(); i++){
         if(monkeys[i].numInspected > first || monkeys[i].numInspected > second){
             if(first < second){
@@ -158,7 +126,6 @@ void run(){
             }
         }
     }
-    cout << "first= " << first << "second= " << second << endl;
     cout << "Output= " << first*second << endl;
 }
 
