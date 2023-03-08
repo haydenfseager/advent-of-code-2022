a4fcfb2dff8fc790f1394ec1739dedbeaf080c49
Hayden Seager <118368802+haydenfseager@users.noreply.github.com>
2023-03-08 13:46:55 -0500

    Finally finished day13 part1

--- day13/day13.1.cpp
+++ day13/day13.1.cpp
@@ -23,17 +23,16 @@ struct Packet{
         storage.push(this);
         Packet* ptr = storage.top();
         for(int i=0; i<line.length(); i++){
+            ptr = storage.top();
             if(line[i] == '['){
-                cout << "Created new packet" << endl;
                 Packet* temp = new Packet();
                 ptr->list.push_back(temp);
                 storage.push(temp);
             }
             else if(line[i] == ']'){
-                cout << "Popped from storage" << endl;
                 storage.pop();
             }
-            else if(line[i] >= '0' && line[i] <= '9'){
+            else if(isdigit(line[i])){
                 //cout << "found num" << endl;
                 if(line[i] == '1' && line[i+1] == '0'){
                     ptr->list.push_back(10);
@@ -66,8 +65,8 @@ void printPacket(Packet* p){
 
 int comparePackets(Packet* p1, Packet* p2){
     for(int i=0; i<min(p1->list.size(), p2->list.size()); i++){
+        //both ints
         if(holds_alternative<int>(p1->list[i]) && holds_alternative<int>(p2->list[i])){
-            //both ints
             int leftVal = get<int>(p1->list[i]);
             int rightVal = get<int>(p2->list[i]);
             if(leftVal < rightVal){
@@ -80,8 +79,8 @@ int comparePackets(Packet* p1, Packet* p2){
                 continue;
             }
         }
+        //both packets
         else if(holds_alternative<Packet*>(p1->list[i]) && holds_alternative<Packet*>(p2->list[i])){
-            //both packets
             Packet* newP1 = get<Packet*>(p1->list[i]);
             Packet* newP2 = get<Packet*>(p2->list[i]);
             int val = comparePackets(newP1,newP2);
@@ -101,13 +100,15 @@ int comparePackets(Packet* p1, Packet* p2){
             int val;
             if(holds_alternative<int>(p1->list[i])){
                 //p1 is int
-                string tempString = "[" + to_string(get<int>(p1->list[i])) + "]";
+                string tempString = "[[" + to_string(get<int>(p1->list[i])) + "]]\n";
+                cout << tempString << endl;
                 Packet* temp = new Packet(tempString);
                 val = comparePackets(temp, p2);
             }
             else{
                 //p2 is int
-                string tempString = "[" + to_string(get<int>(p2->list[i])) + "]";
+                string tempString = "[[" + to_string(get<int>(p2->list[i])) + "]]\n";
+                cout << tempString << endl;
                 Packet* temp = new Packet(tempString);
                 val = comparePackets(p1, temp);
             }
@@ -136,22 +137,25 @@ int comparePackets(Packet* p1, Packet* p2){
 int main(){
     packetNum = 0;
     ifstream fin;
-    fin.open("day13.test.txt");
+    fin.open("day13.txt");
     string line;
     int output = 0;
-    int i = 1;
+    int i=1;
     while(getline(fin, line)){
         //getline(fin, line);
         if(line.length() == 1){
             i++;
+            cout << endl;
             continue;
         }
         else{
             Packet* temp1 = new Packet(line);
             printPacket(temp1);
+            cout << endl;
             getline(fin, line);
             Packet* temp2 = new Packet(line);
             printPacket(temp2);
+            cout << endl;
             output += comparePackets(temp1, temp2)*i;
         }
     }
